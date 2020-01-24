5968bee0d91c926786c08bbc283e16f21cd5c6f5
Yashodhan Singh <yashodhan.singh@unoapp.io>
2020-01-24 14:42:51 +0530

    improve(opts-helpers): remove $addMiddlewareContext

--- src/install.ts
+++ src/install.ts
@@ -66,17 +66,9 @@ export const install: Install<Router | PluginOptions> = (
     context = { ..._context, app }
     return context
   }
-  vue.$addMiddlewareContext = (key: string, value: any) => {
+  vue.$updateMiddlewareContext = (key: string, value: any) => {
     const { to, from, redirect, app } = context
     context = { [key]: value, to, from, redirect, app }
-  }
-  vue.$updateMiddlewareContext = (key: string, value: any) => {
-    if (key === 'app') {
-      throw new BasePluginError(
-        'cannot update internal context property, `key`'
-      )
-    }
-    context[key] = value
   }
 
   /* istanbul ignore next */
